e280101de193455e855fe5a30a09e0c7111f5b3b
Galo <gofer1505@gmail.com>
2022-05-21 18:22:40 +0300

    find next frame cases 1 and 2 handled

--- ex4/VirtualMemory.cpp
+++ ex4/VirtualMemory.cpp
@@ -8,6 +8,11 @@
 
 #define SUCCESS 0
 
+typedef struct{
+	int frameIndex;
+	int frameOffset;
+} FrameAddress;
+
 int readByIndex(int frameIndex, uint64_t frameOffset);
 
 bool isFrameEmpty(int frameIndex);
@@ -27,6 +32,17 @@ word_t firstTranslation(uint64_t pagesTable[]);
 
 word_t secondTranslation(uint64_t pageNum, word_t lastFrame);
 
+/** Finds the first frame in memory that satisfies the condition provided as a boolean function, by
+ * preforming a DFS run on the page tree.
+ *
+ * returns the free frame's index upon success.
+ * returns -1 if no empty frame was found.
+ */
+int findEmptyFrame(int startFrameIndex,
+				   int currDepth,
+				   int* maxFrameVisited,
+				   FrameAddress *parentFrame,
+				   bool (*condition)(int));
 /**
  * Initializes a new frame according to the given frameIndex.
  */
@@ -51,7 +67,7 @@ void VMinitialize(){
  * address for any reason)
  */
 int VMread(uint64_t virtualAddress, word_t* value) {
-    // TODO if the address cannot be mapped to a physic address for any reason
+    // TODO if the address cannot be mapped to a physical address for any reason
     uint64_t physicalAddress = translateToPhysicalAddress(virtualAddress);
     PMread(physicalAddress, value);
     return SUCCESS;
@@ -74,7 +90,7 @@ int VMwrite(uint64_t virtualAddress, word_t value){
 void translateToVirtualOffsets(uint64_t pageTable[], uint64_t pages) {
     for (int i = TABLES_DEPTH - 1; i >= 0; --i) {
         pageTable[i] = pages % PAGE_SIZE; // TODO check if this is the right macro to get the pages: [p1, p2]
-        pages = pages >> OFFSET_WIDTH;
+        pages = pages >> PAGE_SIZE;
     }
 }
 
@@ -127,7 +143,23 @@ word_t firstTranslation(uint64_t pagesTable[]) {
 }
 
 word_t findNextFrame(word_t frame) {
-    return 0;
+	int maxFrameVisited = 0;
+	FrameAddress parentFrame;
+	int emptyFrameIndex = findEmptyFrame(frame,
+										 0,
+										 &maxFrameVisited,
+										 &parentFrame,
+										 isFrameEmpty);
+	if (emptyFrameIndex != FAILURE){
+		writeByIndex(parentFrame.frameIndex, parentFrame.frameOffset,
+												 EMPTY_CELL_VALUE);
+		return emptyFrameIndex;
+	}
+	if(maxFrameVisited < NUM_FRAMES - 1){
+		return maxFrameVisited + 1;
+	}
+	// find page to evict
+	return 0;
 }
 
 
@@ -147,31 +179,52 @@ uint64_t convertIndexesToAddress(uint64_t frameIndex, uint64_t frameOffset){
 	return frameIndex * PAGE_SIZE + frameOffset; // TODO: verify type conversion
 }
 
-/** Finds the first frame in memory that satisfies the condition provided as a boolean function, by
- * preforming a DFS run on the page tree.
- *
- * returns the free frame's index upon success.
- * returns -1 if no empty frame was found.
+
+/**
+ * Reads the content of a given frame index and its offset and returns the result.
  */
-int findSatisfyingFrame(int startFrameIndex, int currDepth, int* maxFrameVisited, bool (*condition)
-(int)) {
+word_t readByIndex(int frameIndex, uint64_t frameOffset) {
+	word_t result;
+	PMread(convertIndexesToAddress(frameIndex, frameOffset), &result);
+	return result;
+}
+
+/**
+ * Write the given value in the a given frame index and its offset.
+ */
+void writeByIndex(int frameIndex, uint64_t frameOffset, word_t value) {
+    PMwrite(convertIndexesToAddress(frameIndex, frameOffset), value);
+}
+
+int findEmptyFrame(int startFrameIndex,
+				   int currDepth,
+				   int *maxFrameVisited,
+				   FrameAddress *parentFrame,
+				   bool (*condition)(int))
+{
 	if (condition(startFrameIndex)){
-		return *maxFrameVisited + 1;
+		return startFrameIndex;
 	}
 	if(startFrameIndex > *maxFrameVisited){
 		*maxFrameVisited = startFrameIndex;
 	}
+
 	if (*maxFrameVisited >= NUM_FRAMES)
 		return FAILURE;
 
-	if (currDepth < (TABLES_DEPTH - 1)){
+	if (currDepth >= (TABLES_DEPTH - 1)){ // TODO: -1?
 		return FAILURE;
 	}
+	parentFrame->frameIndex = startFrameIndex; //TODO: struct containing frame index and offset
 	for (int i = 0; i < PAGE_SIZE; i++){
+		parentFrame->frameOffset = i;
 		word_t cellValue = readByIndex(startFrameIndex, i);
 		if (cellValue != EMPTY_CELL_VALUE){
-			int subSearchResult = findSatisfyingFrame(cellValue, currDepth + 1, maxFrameVisited,
-                                                      condition);
+			int subSearchResult = findEmptyFrame(cellValue,
+												 currDepth + 1,
+												 maxFrameVisited,
+												 parentFrame,
+												 condition);
 			if (subSearchResult != -1){
 				return subSearchResult;
 			}
