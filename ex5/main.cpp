2e492008f4a45811363745ff08faaf00dfd47def
dorelby <dorel.bbez@gmail.com>
2022-06-07 17:08:43 +0300

    added error messages

--- ex5/main.cpp
+++ ex5/main.cpp
@@ -11,35 +11,64 @@
 
 #define STACK 8192
 
+#define PERMISSIONS 0755
 
-#define CGROUPS_PATH "/sys/fs/cgroups/pids"
+
+#define CGROUPS_PIDS "/sys/fs/cgroups/pids"
+#define CGROUPS_PROCS "/cgroup.procs"
+#define CGROUPS_MAX "/pids.max"
+#define CGROUPS_NOTIFY "/notify_on_release"
+#define CGROUPS "/sys/fs/cgroups"
+#define FS "/sys/fs"
+#define CHILD_ID "1"
+
+#define SYSTEM_ERROR_MSG "system error: "
+#define MEMORY_ALLOCATION_ERROR_MSG "failed to allocate memory."
+#define CLONE_ERROR_MSG "error on clone"
+#define MOUNT_ERROR_MSG "error on mount"
+#define SETHOSTNAME_ERROR_MSG "error on sethostname"
+#define CHROOT_ERROR_MSG "error on chroot"
+#define CHDIR_ERROR_MSG "error on chdir"
+#define EXECVP_ERROR_MSG "error on execvp"
+#define WAIT_ERROR_MSG "error on wait"
+
+
+
+/**
+ * prints system error to stderr
+ */
+void systemError(const std::string &string) {
+    std::cerr << SYSTEM_ERROR_MSG + string << std::endl;
+    exit(EXIT_FAILURE);
+}
+
 
 void set_cgroup(char* numProcesses){
-	mkdir(CGROUPS_PATH, 0755); // TODO: check permissions
+    mkdir(CGROUPS_PIDS, PERMISSIONS);
+    std::ofstream procs_file;
+    procs_file.open (CGROUPS_PIDS CGROUPS_PROCS);
+    procs_file << CHILD_ID;
+    procs_file.close();
 
-	std::ofstream procs_file;
-	procs_file.open (CGROUPS_PATH + "/cgroup.procs");
-	procs_file << "1";
-	procs_file.close();
+    std::ofstream max_pids_file;
+    max_pids_file.open (CGROUPS_PIDS CGROUPS_MAX);
+    max_pids_file << numProcesses;
+    max_pids_file.close();
 
-	std::ofstream max_pids_file;
-	max_pids_file.open (CGROUPS_PATH + "/pids.max");
-	max_pids_file << numProcesses;
-	max_pids_file.close();
-
-	std::ofstream notify_file;
-	notify_file.open (CGROUPS_PATH + "/notify_on_release");
-	notify_file << "1";
-	notify_file.close();
+    std::ofstream notify_file;
+    notify_file.open (CGROUPS_PIDS CGROUPS_NOTIFY);
+    notify_file << CHILD_ID;
+    notify_file.close();
 }
 
 void clean_up_cgroup(){
-	remove(CGROUPS_PATH + "/cgroup.procs");
-	remove(CGROUPS_PATH + "/pids.max");
-	remove(CGROUPS_PATH + "/notify_on_release");
-	rmdir(CGROUPS_PATH);
-	rmdir("/sys/fs/cgroups");
-	rmdir("/sys/fs");
+    std::string cgroups_path = std::string(CGROUPS_PIDS);
+    remove(CGROUPS_PIDS CGROUPS_PROCS);
+    remove(CGROUPS_PIDS CGROUPS_MAX);
+    remove(CGROUPS_PIDS CGROUPS_NOTIFY);
+    rmdir(CGROUPS_PIDS);
+    rmdir(CGROUPS);
+    rmdir(FS);
 }
 
 void child(char* args[]) {
@@ -49,22 +78,46 @@ void child(char* args[]) {
     char* pathToProgramToRun = args[4];
     char* argsForProgram = args[5];
 
-    sethostname(newHostName, strlen(newHostName)); // set new host name
-    chroot(newFilesystemDirectory); // set new filesystem directory
-	chdir(newFilesystemDirectory);  // change working directory to newly set filesystem directory
-    mount("proc", "/proc", "proc", 0, 0); // new process will be same inside proc
-	set_cgroup(numProcesses);
+    if (sethostname(newHostName, strlen(newHostName)) < 0) { // set new host name
+        systemError(SETHOSTNAME_ERROR_MSG);
+    }
+
+    if (chroot(newFilesystemDirectory) < 0) { // set new filesystem directory
+        systemError(CHROOT_ERROR_MSG);
+    }
+    if (chdir(newFilesystemDirectory) < 0) { // change working directory to newly set filesystem directory
+        systemError(CHDIR_ERROR_MSG);
+    }
+
+    if (mount("proc", "/proc", "proc", 0, 0) < 0) { // new process will be same inside proc
+        systemError(MOUNT_ERROR_MSG);
+    }
+
+    set_cgroup(numProcesses); // TODO errors on opening files?
 
     char* argsChild[] ={pathToProgramToRun, argsForProgram + 1, (char *)0};
-    execvp(pathToProgramToRun, argsChild);
+    if(execvp(pathToProgramToRun, argsChild) < 0) {
+        systemError(EXECVP_ERROR_MSG);
+    }
 }
 
 int main(int argc, char* argv[]) {
-    char* stack = new char [STACK]; // child's stack
-    int child_pid = clone(reinterpret_cast<int (*)(void *)>(child), // TODO check if ok
+    char* stack = new (std::nothrow) char [STACK]; // child's stack
+    if(stack == nullptr) {
+        systemError(MEMORY_ALLOCATION_ERROR_MSG);
+    }
+
+    if(clone(reinterpret_cast<int (*)(void *)>(child), // TODO check if reinterpret_cast ok
                           stack + STACK,
-                          CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNS | SIGCHLD, argv);
-    wait(nullptr);
-	clean_up_cgroup();
-    umount("proc"); // TODO check if ok
+                          CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNS | SIGCHLD, argv) < 0) {
+        systemError(CLONE_ERROR_MSG);
+    }
+
+    if(wait(nullptr) < 0) {
+        systemError(WAIT_ERROR_MSG);
+
+    }
+
+    clean_up_cgroup();
+    umount("proc"); // TODO check if ok + if need error msg
 }
